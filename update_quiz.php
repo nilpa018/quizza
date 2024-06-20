2ffea2fdec18c639bd2adccc39ffcf4a94ea8de8
maimai <mailys.andrieu.simplon@gmail.com>
2024-06-20 20:30:08 +0200

    correction erreur

--- update_quiz.php
+++ update_quiz.php
@@ -27,26 +27,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // suppression des questions
     $sql1="DELETE FROM quiz_questions
     WHERE quiz_id = $quizId";
-    if($conn->query($sql1) === true){
-            // insérer les liaisons des questions en lien au quizz
-        $sql2= "INSERT INTO quiz_questions(quiz_id,question_id)
-        VALUES";  
-        $placeholders = array_fill(0, count($questionsId), '('.$quizId.',?)'); 
-        $sql2 .= implode(',', $placeholders);
-
-        $stmt = $conn->prepare($sql2);
-        if ($stmt) {
-            $stmt->bind_param( str_repeat('i', count($questionsId)), ...$questionsId); 
-            if ($stmt->execute()) {
-                header('location: quizzes.php');
-            } else {
-                echo "Erreur : " . $sql . "<br>" . $stmt->error;
-            };
-        } else {
-            echo "Erreur lors de la préparation de la requête : " . $conn->error;
-        }
+    $conn->query($sql1);
+    if($conn->query($sql1) != true){
+        echo "error".$sql1."<br>".$conn->error;
     }
-    else{echo "error".$sql1."<br>".$conn->error;}
-    
+   
+           // insérer les liaisons des questions en lien au quizz
+           $sql2= "INSERT INTO quiz_questions(quiz_id,question_id)
+           VALUES";  
+           $placeholders = array_fill(0, count($questionsId), '('.$quizId.',?)'); 
+           $sql2 .= implode(',', $placeholders);
+   
+           $stmt = $conn->prepare($sql2);
+           if ($stmt) {
+               $stmt->bind_param( str_repeat('i', count($questionsId)), ...$questionsId); 
+               if ($stmt->execute()) {
+                   header('location: quizzes.php');
+               } else {
+                   echo "Erreur : " . $sql . "<br>" . $stmt->error;
+               };
+           } else {
+               echo "Erreur lors de la préparation de la requête : " . $conn->error;
+           }
    
 }
